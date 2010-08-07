5695cc1c841b6d5835835c7a45feee3833c0a4f3
Steve Streza <stevestreza@gmail.com>
2010-08-07 16:20:52 -0700

    Updated example to use authenticators

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,14 @@
 var sys = require('sys');
 var pop3 = require('../lib/pop3');
 
-var client = new pop3.POPClient(110, "localhost.com", "steve", "test");
+var auth = pop3.POPClient.authentication.apop("steve", "test");
+
+var client = new pop3.POPClient(110, "localhost.com", auth);
 client.addListener("authenticate", function(){
 	sys.puts("we have authenticated!");
+	client.sendAndWait(["STAT"], function(line){
+		sys.puts("stat: " + line);
+		client.disconnect();
+	})
 })
 client.connect();
-
-setTimeout(1000, function(){
-	client.disconnect();
-})
